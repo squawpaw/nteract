a2a7c74b0081bebd1c6e32800a6872c9f1f2ee10
Kyle Kelley <rgbkrk@gmail.com>
2016-08-30 19:12:12 -0700

    Introduce a Plotly transform.

--- src/notebook/components/transforms/index.js
+++ src/notebook/components/transforms/index.js
@@ -1,8 +1,14 @@
 import { displayOrder, transforms } from 'transformime-react';
 
 import VictoryTransform from './victory';
+import PlotlyTransform from './plotly';
 
-const defaultDisplayOrder = displayOrder.splice(0, 0, VictoryTransform.MIMETYPE);
-const defaultTransforms = transforms.set(VictoryTransform.MIMETYPE, VictoryTransform);
+const defaultDisplayOrder = displayOrder
+  .splice(0, 0, VictoryTransform.MIMETYPE)
+  .splice(0, 0, PlotlyTransform.MIMETYPE);
+
+const defaultTransforms = transforms
+  .set(VictoryTransform.MIMETYPE, VictoryTransform)
+  .set(PlotlyTransform.MIMETYPE, PlotlyTransform);
 
 export { defaultDisplayOrder, defaultTransforms };
